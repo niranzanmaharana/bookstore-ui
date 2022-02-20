ef9e1880abb41fcb755406eeabe549a31b620f3e
Niranjan Maharana <niranzan.in@gmail.com>
2022-02-20 12:00:59 +0530

    feature 09

--- src/app/components/cart-detail/cart-detail.component.ts
+++ src/app/components/cart-detail/cart-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-detail',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-detail.component.scss']
 })
 export class CartDetailComponent implements OnInit {
-
-  constructor() { }
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+  
+  constructor(private _cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cartDetails();
+  }
+
+  cartDetails() {
+    this.cartItems = this._cartService.cartItems;
+    // Subscribe to the events published by cart service
+    this._cartService.totalPrice.subscribe(data => this.totalPrice = data);
+    this._cartService.totalQuantity.subscribe(data => this.totalQuantity = data);
+
+    this._cartService.calculateTotalPrice();
   }
 
 }
